50fc3df239a0dd9e0d8ac1f31e7fff23ce430f71
Shayla White <shayladwhite1@gmail.com>
2024-12-16 00:29:09 -0800

    updated the service class to implement the api random integer

--- src/main/java/com/gemsoflifegame/service/GameService.java
+++ src/main/java/com/gemsoflifegame/service/GameService.java
@@ -1,4 +1,17 @@
 package com.gemsoflifegame.service;
 
+@Service
 public class GameService {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private final String API_URL = "https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain";
+
+    public int[] generateNumbers() {
+        String response = restTemplate.getForObject(API_URL, String.class);
+        String[] numbers = response.split("\n");
+        return new int[]{Integer.parseInt(numbers[0]), Integer.parseInt(numbers[1]),
+                Integer.parseInt(numbers[2]), Integer.parseInt(numbers[3])};
+    }
+}
 }
