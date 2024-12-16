ecdf9c3088580735eb672e413815c968e40fdd5c
Shayla White <shayladwhite1@gmail.com>
2024-12-16 00:06:17 -0800

    updated pom, properties, and app

--- src/main/java/com/gemsoflifegame/GemsoflifeGameApplication.java
+++ src/main/java/com/gemsoflifegame/GemsoflifeGameApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GemsoflifeGameApplication {
 
 	public static void main(String[] args) {
+		// This runs the Spring Boot application
 		SpringApplication.run(GemsoflifeGameApplication.class, args);
 	}
-
 }
